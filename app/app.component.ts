7d033f5d1f87fc6101b2425413b52ec8486cc994
Nathan Walker <walkerrunpdx@gmail.com>
2016-11-20 19:49:32 -0800

    feat: menu setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from "@angular/core";
+import { topmost } from 'ui/frame';
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
 export class AppComponent {
+    public items: any[] = [
+        { title: 'test' },
+        { title: 'test 2' },
+        { title: 'test 3' }
+    ]
     public counter: number = 16;
+
+    constructor() {
+        topmost().ios.controller.navigationBar.barStyle = 1;
+    }
 
     public get message(): string {
         if (this.counter > 0) {
